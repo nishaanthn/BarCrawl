6f5e9ee2f786f0916ba72a97a9b6f6649b8e5f26
Rosbel Sanroman <rlsanroman@gmail.com>
2013-12-04 09:56:56 -0600

    Updated the website

---
+++ BarCrawlWebsite/js/map.js
@@ -0,0 +1,169 @@
+var DaisyDukesCoords = [
+	new google.maps.LatLng(30.61708894196927,-96.34713515639305),
+	new google.maps.LatLng(30.617166269408624,-96.34704798460007),
+	new google.maps.LatLng(30.617240134367602,-96.34713247418404 ),
+	new google.maps.LatLng(30.617218205713794,-96.34716063737869),
+	new google.maps.LatLng(30.617218205713794,-96.34760051965714 ),
+	new google.maps.LatLng(30.617109716511283,-96.34760320186615 ),
+	new google.maps.LatLng(30.617113178934506,-96.34716600179672 )
+];
+
+var DaisyDukes = new google.maps.Polygon({
+	paths: DaisyDukesCoords,
+	strokeColor: '#FF0000',
+	strokeOpacity: 0.8,
+	strokeWeight: 1,
+	fillColor: '#FF0000',
+	fillOpacity: 0.35
+});
+
+var MadHattersCoords = [
+	new google.maps.LatLng(30.6179908992301,-96.34612664580345),
+	new google.maps.LatLng(30.61803071672741,-96.3460810482502),
+	new google.maps.LatLng(30.61830713049643,-96.34637475013733),
+	new google.maps.LatLng(30.618271929621873,-96.34642101824284)
+];
+
+var MadHatters = new google.maps.Polygon({
+	paths: MadHattersCoords,
+	strokeColor: '#FF0000',
+	strokeOpacity: 0.8,
+	strokeWeight: 1,
+	fillColor: '#FF0000',
+	fillOpacity: 0.35
+});
+
+var DixieChickenCoords = [
+	new google.maps.LatLng(30.617814894154513,-96.34634122252464),
+	new google.maps.LatLng(30.617857597054694,-96.34628087282181),
+	new google.maps.LatLng(30.617846055732183,-96.34627014398575),
+	new google.maps.LatLng(30.61789568340927,-96.3462084531784),
+	new google.maps.LatLng(30.618226918648908,-96.3465490937233),
+	new google.maps.LatLng(30.61814497554181,-96.34664162993431),
+	new google.maps.LatLng(30.618084960827844,-96.34660139679909),
+	new google.maps.LatLng(30.618078036050733,-96.34661614894867)
+];
+
+var DixieChicken = new google.maps.Polygon({
+	paths: DixieChickenCoords,
+	strokeColor: '#FFFF00',
+	strokeOpacity: 0.8,
+	strokeWeight: 1,
+	fillColor: '#FFFF00',
+	fillOpacity: 0.35
+});
+
+var DuddleysDrawCoords = [
+	new google.maps.LatLng(30.617912995383662,-96.34622052311897),
+	new google.maps.LatLng(30.61796608541914,-96.34615480899811),
+	new google.maps.LatLng(30.618276546130758,-96.34648606181145),
+	new google.maps.LatLng(30.61822576452113,-96.34654775261879)
+];
+
+var DuddleysDraw = new google.maps.Polygon({
+	paths: DuddleysDrawCoords,
+	strokeColor: '#00FF00',
+	strokeOpacity: 0.8,
+	strokeWeight: 1,
+	fillColor: '#00FF00',
+	fillOpacity: 0.35
+});
+
+
+var OBannonsCoords = [
+	new google.maps.LatLng(30.617240134367602,-96.34713649749756),
+	new google.maps.LatLng(30.617218205713794,-96.34716063737869),
+	new google.maps.LatLng(30.61721935985358,-96.34743824601173),
+	new google.maps.LatLng(30.617243596786167,-96.34743690490723),
+	new google.maps.LatLng(30.617244750925675,-96.34745836257935),
+	new google.maps.LatLng(30.6173809392896,-96.34728536009789)
+];
+
+var OBannons = new google.maps.Polygon({
+	paths: OBannonsCoords,
+	strokeColor: '#FF0000',
+	strokeOpacity: 0.8,
+	strokeWeight: 1,
+	fillColor: '#FF0000',
+	fillOpacity: 0.35
+});
+
+var LogansCoords = [
+	new google.maps.LatLng(30.618786669004244,-96.34611994028091),
+	new google.maps.LatLng(30.618862840967342,-96.34621247649193),
+	new google.maps.LatLng(30.618694339271606,-96.34640157222748),
+	new google.maps.LatLng(30.61862278366827,-96.34632110595703)
+];
+
+var Logans = new google.maps.Polygon({
+	paths: LogansCoords,
+	strokeColor: '#00FF00',
+	strokeOpacity: 0.8,
+	strokeWeight: 1,
+	fillColor: '#00FF00',
+	fillOpacity: 0.35
+});
+
+var SakeCoords = [
+	new google.maps.LatLng(30.61866317796698,-96.34595766663551),
+	new google.maps.LatLng(30.618509679542186,-96.34613737463951),
+	new google.maps.LatLng(30.618552382135938,-96.3461896777153),
+	new google.maps.LatLng(30.6187116511032,-96.34600728750229)
+];
+
+var Sake = new google.maps.Polygon({
+	paths: SakeCoords,
+	strokeColor: '#FFFF00',
+	strokeOpacity: 0.8,
+	strokeWeight: 1,
+	fillColor: '#FFFF00',
+	fillOpacity: 0.35
+});
+
+var HookaCoords = [
+	new google.maps.LatLng(30.61881552202762,-96.34558886289597),
+	new google.maps.LatLng(30.618853608005313,-96.3456304371357 ),
+	new google.maps.LatLng(30.61870934285915,-96.3458114862442 ),
+	new google.maps.LatLng(30.618672410947173,-96.34577661752701)
+];
+
+var Hooka = new google.maps.Polygon({
+	paths: HookaCoords,
+	strokeColor: '#FFFF00',
+	strokeOpacity: 0.8,
+	strokeWeight: 1,
+	fillColor: '#FFFF00',
+	fillOpacity: 0.35
+});
+
+var FoundationCoords = [
+	new google.maps.LatLng(30.618853608005313,-96.3456304371357),
+	new google.maps.LatLng(30.61870934285915,-96.3458114862442),
+	new google.maps.LatLng(30.618755507729315,-96.34585842490196),
+	new google.maps.LatLng(30.618896310447365,-96.34568676352501 )
+];
+
+var Foundation = new google.maps.Polygon({
+	paths: FoundationCoords,
+	strokeColor: '#00FF00',
+	strokeOpacity: 0.8,
+	strokeWeight: 1,
+	fillColor: '#00FF00',
+	fillOpacity: 0.35
+});
+
+var SocialCoords = [
+	new google.maps.LatLng(30.618755507729315,-96.34585842490196),
+	new google.maps.LatLng(30.618896310447365,-96.34568676352501),
+	new google.maps.LatLng(30.618958632896494,-96.3457578420639),
+	new google.maps.LatLng(30.618814367906843,-96.34591341018677)
+];
+
+var Social = new google.maps.Polygon({
+	paths: SocialCoords,
+	strokeColor: '#FFFF00',
+	strokeOpacity: 0.8,
+	strokeWeight: 1,
+	fillColor: '#FFFF00',
+	fillOpacity: 0.35
+});
